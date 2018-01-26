ab902e5dd7d09e6f6f1b2d6bedf956f79e914d2e
CHAPMAN Peter <Peter.CHAPMAN@dhsoha.state.or.us>
2018-01-26 05:38:49 -0800

    test cases

--- src/main/java/com/chapman/ftfcu/service/UserService.java
+++ src/main/java/com/chapman/ftfcu/service/UserService.java
@@ -2,13 +2,15 @@ package com.chapman.ftfcu.service;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.chapman.ftfcu.model.User;
 
-/**
 /**
  * Created by pchapman@easystreet.net on Jan 12, 2018
  */
 
+@Service
 public interface UserService {
 
 	List<User> findAll();
